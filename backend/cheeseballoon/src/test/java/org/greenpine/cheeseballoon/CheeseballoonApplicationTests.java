28b90c6b1156abe385d913292912136b5c989039
kwonhyeokgeun <xkhg0611x@naver.com>
2024-02-23 00:42:09 +0900

    init

---
+++ backend/cheeseballoon/src/test/java/org/greenpine/cheeseballoon/CheeseballoonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greenpine.cheeseballoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseballoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
