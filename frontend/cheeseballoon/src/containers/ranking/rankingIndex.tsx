8596a11036827de25a9e76c6c91c881e1197f2a6
kwonhyeokgeun <xkhg0611x@naver.com>
2024-03-08 18:49:32 +0900

    feature : live세팅

--- frontend/cheeseballoon/src/containers/ranking/rankingIndex.tsx
+++
@@ -1,9 +0,0 @@
-import style from "src/containers/ranking/rankingIndex.module.scss";
-
-export default function Ranking() {
-  return (
-    <div className={style.ranking}>
-      <p>안녕하세요, ranking 페이지입니다.</p>
-    </div>
-  );
-}
