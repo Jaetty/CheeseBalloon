84619f45a20b4c77860662f708c81dab10ac4ec2
kwonhyeokgeun <xkhg0611x@naver.com>
2024-03-09 00:52:03 +0900

    feature : live 목업데이터 조회

---
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/live/application/port/in/LiveUsecase.java
@@ -0,0 +1,11 @@
+package org.greenpine.cheeseballoon.live.application.port.in;
+
+import org.greenpine.cheeseballoon.live.application.port.in.dto.FindLiveReqDto;
+import org.greenpine.cheeseballoon.live.application.port.out.dto.FindLiveResDto;
+
+import java.util.List;
+
+public interface LiveUsecase {
+
+    List<FindLiveResDto> readLive(FindLiveReqDto findLiveReqDto);
+}
