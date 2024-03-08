22bf8822b78f736859741f0d36e3538954ffd512
kwonhyeokgeun <xkhg0611x@naver.com>
2024-03-08 18:49:32 +0900

    feature : live세팅

--- README.md
+++
@@ -1,5 +0,0 @@
-# CheeseBalloon
-치즈벌룬 개발 레포지토리
-
----
-초대 받은 멤버 : rudgns9334
--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/example/application/port/in/AnotherUsecase.java
+++
@@ -1,5 +0,0 @@
-package org.greenpine.cheeseballoon.example.application.port.in;
-
-public interface AnotherUsecase { //이건 추가적인 유스케이스 예시(내용은 작성안함)
-    //또다른 CURD 구현
-}
