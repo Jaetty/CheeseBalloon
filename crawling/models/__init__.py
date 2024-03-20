d9447cc9936675a7e02340fd37d1f0639c4a8d60
9gi_coach21 <9gi_coach21@ssafy.com>
2024-03-20 11:46:38 +0900

    feat: 크롤러 cycle logs 테이블 추가

--- crawling/models/__init__.py
+++ crawling/models/__init__.py
@@ -3,3 +3,4 @@ from .streamer_logs import StreamerLog
 from .lives import Live
 from .live_logs import LiveLog
 from .categories import Category
+from .cycle_logs import CycleLog
---
+++ crawling/models/cycle_logs.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import Column, BigInteger, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from config.database import Base
+
+class CycleLog(Base):
+    __tablename__ = 'live_logs'
+
+    cycle_log_id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    afreeca_viewer_cnt = Column(Integer, nullable=False)
+    chzzk_viewer_cnt = Column(Integer, nullable=False)
+    category_id = Column(Integer, nullable=False)
+    live_dt = Column(DateTime, nullable=False, default= datetime.today())
+
+    live_logs = relationship("LiveLog", back_populates="cycle_log")
