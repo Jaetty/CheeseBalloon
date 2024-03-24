0da71fcc2ffd35f0d443f282e3fa2e78c36fcc4e
kwonhyeokgeun <xkhg0611x@naver.com>
2024-03-24 18:14:47 +0900

    feature : 로그추가

--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/CheeseballoonApplication.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/CheeseballoonApplication.java
@@ -2,6 +2,7 @@ package org.greenpine.cheeseballoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 public class CheeseballoonApplication {
--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/global/config/CorsConfig.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/global/config/CorsConfig.java
@@ -1,8 +1,11 @@
 package org.greenpine.cheeseballoon.global.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import javax.servlet.Filter;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
