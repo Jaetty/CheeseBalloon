a433ec457a76e36b030d938b093a8cf3fd9fc055
kwonhyeokgeun <xkhg0611x@naver.com>
2024-03-25 17:43:01 +0900

    feature : jwt 토큰 생성 및 security 코드 작성 완료

---
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package org.greenpine.cheeseballoon.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String origin = httpRequest.getHeader("Origin");
+        System.out.println("Origin: " + origin);
+        filterChain.doFilter(request, response);
+    }
+
+    // Request Header에서 토큰 정보 추출
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
---
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/global/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package org.greenpine.cheeseballoon.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //CSRF, CORS
+        http.csrf((csrf) -> csrf.disable());
+        http.cors(Customizer.withDefaults());
+
+        //세션 관리 상태 없음으로 구성, Spring Security가 세션 생성 or 사용 X
+        http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS));
+
+        //JwtAuthFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+        http.addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        // 권한 규칙 작성
+        http.authorizeHttpRequests(authorize -> authorize
+                //.requestMatchers().permitAll()
+                //모든 경로에 대한 인증처리는 Pass
+                .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+}
