3011c9568b20382bdb53b4cfb91593fc30cff0bd
kwonhyeokgeun <xkhg0611x@naver.com>
2024-04-01 14:59:15 +0900

    feature : ranking 틀제작

--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/ranking/application/port/in/dto/FindFollowRankingReqDto.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/ranking/application/port/in/dto/FindFollowRankingReqDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class FindRankingReqDto {
+public class FindFollowRankingReqDto {
 
     int limit;
     int offset;
---
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/ranking/application/port/out/RankingPort.java
@@ -0,0 +1,10 @@
+package org.greenpine.cheeseballoon.ranking.application.port.out;
+
+import org.greenpine.cheeseballoon.ranking.application.port.in.dto.FindFollowRankingReqDto;
+import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindFollowRankingResDto;
+
+import java.util.List;
+
+public interface RankingPort {
+    List<FindFollowRankingResDto> findFollowRanking(FindFollowRankingReqDto reqDto);
+}
