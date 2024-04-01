28270debb69adec191f303cb142de78e9564754b
Suou <baewc95@gmail.com>
2024-04-01 16:22:06 +0900

    commit 지우기중

--- frontend/cheeseballoon/src/containers/detail/detailSelectDate.tsx
+++ frontend/cheeseballoon/src/containers/detail/detailSelectDate.tsx
@@ -1,15 +1,19 @@
-'use client'
+"use client";
 
 import { useParams, useRouter } from "next/navigation";
 import style from "./detailSelectDate.module.scss";
 
 export default function DetailSelectDate() {
-  const { id, category } = useParams();
+  const { id, category, date } = useParams();
   const router = useRouter();
   const handleChangeDate = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newDate = e.target.value;
-    router.push(category ? `/detail/${id}/${category}/${newDate}` : `/detail/${id}/viewer/${newDate}`);
 
+    router.push(
+      category
+        ? `/detail/${id}/${category}/${newDate}`
+        : `/detail/${id}/viewer/${newDate}`
+    );
   };
 
   return (
@@ -19,6 +23,7 @@ export default function DetailSelectDate() {
           handleChangeDate(e);
         }}
         className={style.button}
+        value={date || 7}
       >
         <option value={7}>7일</option>
         <option value={14}>14일</option>
