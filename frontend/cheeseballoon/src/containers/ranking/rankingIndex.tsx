ea07db886260c61fd598e40d1a4c28339fd65dbb
myikn <myikn.999@gmail.com>
2024-04-08 05:31:49 +0900

    Feat: Fetch 동작구현, 세부랭킹 틀 완성, Link 연결

--- frontend/cheeseballoon/src/containers/ranking/rankingIndex.tsx
+++ frontend/cheeseballoon/src/containers/ranking/rankingIndex.tsx
@@ -3,27 +3,75 @@
 import style from "src/containers/ranking/rankingIndex.module.scss";
 import DaySelect from "src/components/ranking/dayselect";
 import PlatformSelect from "src/components/ranking/platformselect";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import RankingIndex from "src/components/ranking/rankingIndex";
+import { RankingData } from "src/types/type";
 
 export default function Ranking() {
   const [date, setDate] = useState(1);
   const [platform, setPlatform] = useState("option1");
-  const title: (
-    | "팔로워 수"
-    | "평균 시청자 수"
-    | "최고 시청자 수"
-    | "총 방송시간"
-    | "시청률"
-    | "실시간 LIVE"
-  )[] = [
-    "팔로워 수",
-    "평균 시청자 수",
-    "최고 시청자 수",
-    "총 방송시간",
-    "시청률",
-    "실시간 LIVE",
-  ];
+  const title = useMemo(
+    () => [
+      "팔로워 수",
+      "평균 시청자 수",
+      "최고 시청자 수",
+      "총 방송시간",
+      "시청률",
+      "실시간 LIVE",
+    ],
+    []
+  );
+  const [rankingData, setRankingData] = useState<RankingData>({});
+  const fetchRankingData = async (
+    rankingTitle: string,
+    selectedDate: number,
+    selectedPlatform: string
+  ) => {
+    let url;
+    switch (rankingTitle) {
+      case "팔로워 수":
+        url = `${process.env.NEXT_PUBLIC_LIVE_API}?offset=1&limit=10&date=${selectedDate}&platform=${selectedPlatform}`;
+        break;
+      case "평균 시청자 수":
+        url = `${process.env.NEXT_PUBLIC_LIVE_API}?offset=2&limit=10&date=${selectedDate}&platform=${selectedPlatform}`;
+        break;
+      case "최고 시청자 수":
+        url = `${process.env.NEXT_PUBLIC_LIVE_API}?offset=3&limit=10&date=${selectedDate}&platform=${selectedPlatform}`;
+        break;
+      case "총 방송시간":
+        url = `${process.env.NEXT_PUBLIC_LIVE_API}?offset=4&limit=10&date=${selectedDate}&platform=${selectedPlatform}`;
+        break;
+      case "시청률":
+        url = `${process.env.NEXT_PUBLIC_LIVE_API}?offset=5&limit=10&date=${selectedDate}&platform=${selectedPlatform}`;
+        break;
+      case "실시간 LIVE":
+        url = `${process.env.NEXT_PUBLIC_LIVE_API}?offset=6&limit=10&date=${selectedDate}&platform=${selectedPlatform}`;
+        break;
+      default:
+        url = undefined;
+    }
+
+    function noop() {}
+
+    if (url) {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setRankingData((prevData) => ({
+          ...prevData,
+          [rankingTitle]: data,
+        }));
+      } catch (error) {
+        noop();
+      }
+    }
+  };
+
+  useEffect(() => {
+    title.forEach((titleItem) => {
+      fetchRankingData(titleItem, date, platform);
+    });
+  }, [date, platform, title]);
 
   const chunkSize = 3;
   const chunks = [];
@@ -46,7 +94,11 @@ export default function Ranking() {
         {chunks.map((chunk, index) => (
           <div key={index} className={style.container}>
             {chunk.map((titleItem) => (
-              <RankingIndex key={titleItem} title={titleItem} />
+              <RankingIndex
+                key={titleItem}
+                title={titleItem}
+                data={rankingData[titleItem]?.data}
+              />
             ))}
           </div>
         ))}
--- frontend/cheeseballoon/src/containers/ranking/subrankingIndex.tsx
+++ frontend/cheeseballoon/src/containers/ranking/subrankingIndex.tsx
@@ -5,32 +5,50 @@ import DaySelect from "src/components/ranking/dayselect";
 import PlatformSelect from "src/components/ranking/platformselect";
 import Subrank from "src/components/ranking/subrank";
 import SubrankAll from "src/components/ranking/subrankAll";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { SubRankingData } from "src/types/type";
 
 export default function Ranking() {
   const [date, setDate] = useState(1);
   const [platform, setPlatform] = useState("option1");
-  // 데이터 상태
-  // const [data, setData] = useState(null);
+  const [num, setNum] = useState(1);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     // 예: const response = await fetch(`api/data?date=${date}&platform=${platform}`);
-  //     // const newData = await response.json();
-  //     // setData(newData);
-  //   };
+  const pathname = usePathname()?.split("/").pop() || "";
+  const mapping: Record<string, string> = {
+    follow: "팔로워 수",
+    average: "평균 시청자 수",
+    topview: "최고 시청자 수",
+    time: "총 방송시간",
+    rating: "시청률",
+    live: "실시간 LIVE",
+  };
+  const [data, setData] = useState<SubRankingData>();
 
-  //   fetchData();
-  // }, [date, platform]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_LIVE_API}?offset=${num}&limit=15&date=${date}&platform=${platform}`
+      );
+      const newData = await response.json();
+      setData(newData);
+    };
+
+    fetchData();
+  }, [date, platform, num]);
+
+  const subrankData = data && data.data?.slice(0, 3);
+  const subrankAllData = data && data.data?.slice(3);
+
   return (
     <div className={style.ranking}>
-      <p className={style.title}>팔로워 수</p>
+      <p className={style.title}>{mapping[pathname]}</p>
       <div className={style.detail_menu}>
         <DaySelect setDate={setDate} />
         <PlatformSelect setPlatform={setPlatform} />
       </div>
-      <Subrank />
-      <SubrankAll />
+      <Subrank data={subrankData} />
+      <SubrankAll data={subrankAllData} />
     </div>
   );
 }
