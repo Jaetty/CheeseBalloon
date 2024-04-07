104bf931d8b0340dd42d6a71eb0ef319c0a39479
myikn <myikn.999@gmail.com>
2024-04-08 05:28:52 +0900

    Update: Types 항목 추가

--- frontend/cheeseballoon/src/types/type.tsx
+++ frontend/cheeseballoon/src/types/type.tsx
@@ -2,3 +2,55 @@ export interface ToggleStateType {
   value: boolean;
   toggle: () => void;
 }
+
+export interface LiveData {
+  streamId: number;
+  liveId: number;
+  name: string;
+  title: string;
+  thumbnailUrl: string;
+  platform: string;
+  profileUrl: string;
+  category: string;
+  viewerCnt: number;
+  streamUrl: string;
+  channelUrl: string;
+}
+
+export interface RankingData {
+  [title: string]: {
+    status: string;
+    message: string;
+    data: Array<{
+      streamId: number;
+      liveId: number;
+      name: string;
+      title: string;
+      thumbnailUrl: string;
+      platform: string;
+      profileUrl: string;
+      category: string;
+      viewerCnt: number;
+      streamUrl: string;
+      channelUrl: string;
+    }>;
+  };
+}
+
+export interface SubRankingData {
+  status: string;
+  message: string;
+  data: Array<{
+    streamId: number;
+    liveId: number;
+    name: string;
+    title: string;
+    thumbnailUrl: string;
+    platform: string;
+    profileUrl: string;
+    category: string;
+    viewerCnt: number;
+    streamUrl: string;
+    channelUrl: string;
+  }>;
+}
