76f28d030a245bc47c2cd00fbf4ca3449f114055
sangjae <dlbia2009@gmail.com>
2024-04-14 12:38:39 +0900

    feature : 시청자 수 diff 값 추가

--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/ranking/application/port/in/RankingUsecase.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/ranking/application/port/in/RankingUsecase.java
@@ -1,7 +1,7 @@
 package org.greenpine.cheeseballoon.ranking.application.port.in;
 
 import org.greenpine.cheeseballoon.ranking.application.port.in.dto.FindFollowRankingReqDto;
-import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindAvgViewerRankResDtoInterface;
+import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindAvgViewerRankingResDto;
 import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindFollowRankingResDto;
 
 import java.util.List;
@@ -9,5 +9,5 @@ import java.util.List;
 public interface RankingUsecase {
     List<FindFollowRankingResDto> findFollowRanking(FindFollowRankingReqDto reqDto);
 
-    List<FindAvgViewerRankResDtoInterface> findAvgViewerRanking(int limit, int offset, int date, char platform);
+    List<FindAvgViewerRankingResDto> findAvgViewerRanking(int date, char platform);
 }
