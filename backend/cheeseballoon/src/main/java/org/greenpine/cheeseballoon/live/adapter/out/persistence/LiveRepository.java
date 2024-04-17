c1733b5a4d8bd1d20abf3eddf0399746ec11b41f
sangjae <dlbia2009@gmail.com>
2024-04-17 15:54:22 +0900

    fix : 랭킹 repo의 boolean 리턴으로 수정

--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/live/adapter/out/persistence/LiveRepository.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/live/adapter/out/persistence/LiveRepository.java
@@ -21,7 +21,7 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
 
     // 모든 플랫폼에서 시청자 수 기준으로 가져오는 sql
     // 나중에 로그인 했을 때 member_id값을 토큰에서 가져와서 member_id 값에 맞게 조회하게 만들어야함.
-    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS averageViewer, case when b.bookmark_id > 0 then TRUE else FALSE END AS bookmark\n" +
+    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS averageViewer, case when b.bookmark_id > 0 then 'TRUE' else 'FALSE' END AS bookmark\n" +
             "FROM \n" +
             "(SELECT ROW_NUMBER() OVER(ORDER BY viewer_cnt DESC) AS rank, live.live_id, live.streamer_id, live.live_origin_id, live.stream_url, live.thumbnail_url, live_log.live_log_id ,live_log.cycle_log_id, live_log.category_id, live_log.title, round(avg(live_log.viewer_cnt),0) AS viewer_cnt, s.name, s.platform, s.profile_url\n" +
             "FROM lives AS live\n" +
@@ -37,7 +37,7 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
     List<FindAvgViewerRankResDtoInterface> findAllAvgViewerRanking(String beforeDay, String today, Long memberId);
 
     // 특정 플랫폼에서 시청자 수 기준으로 가져오는 sql
-    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS averageViewer, case when b.bookmark_id > 0 then TRUE else FALSE END AS bookmark\n" +
+    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS averageViewer, case when b.bookmark_id > 0 then 'TRUE' else 'FALSE' END AS bookmark\n" +
             "FROM \n" +
             "(SELECT ROW_NUMBER() OVER(ORDER BY viewer_cnt DESC) AS rank, live.live_id, live.streamer_id, live.live_origin_id, live.stream_url, live.thumbnail_url, live_log.live_log_id ,live_log.cycle_log_id, live_log.category_id, live_log.title, round(avg(live_log.viewer_cnt),0) AS viewer_cnt, s.name, s.platform, s.profile_url\n" +
             "FROM lives AS live\n" +
@@ -53,7 +53,7 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
     List<FindAvgViewerRankResDtoInterface> findAvgViewerRankingByPlatform(String beforeDay, String today, char platform, Long memberId);
 
     // 모든 플랫폼에서 가장 많은 시청자 수 순위로 가져오기
-    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS topViewer, case when b.bookmark_id > 0 then TRUE else FALSE END AS bookmark\n" +
+    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS topViewer, case when b.bookmark_id > 0 then 'TRUE' else 'FALSE' END AS bookmark\n" +
             "FROM \n" +
             "(SELECT ROW_NUMBER() OVER(ORDER BY viewer_cnt DESC) AS rank, live.live_id, live.streamer_id, live.live_origin_id, live.stream_url, live.thumbnail_url, live_log.live_log_id ,live_log.cycle_log_id, live_log.category_id, live_log.title, round(max(live_log.viewer_cnt),0) AS viewer_cnt, s.name, s.platform, s.profile_url\n" +
             "FROM lives AS live\n" +
@@ -69,7 +69,7 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
     List<FindTopViewerRankResDtoInterface> findAllTopViewerRanking(String beforeDay, String today, Long memberId);
 
     // 특정 플랫폼에서 가장 많은 시청자 수 기준으로 가져오는 sql
-    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS topViewer, case when b.bookmark_id > 0 then TRUE else FALSE END AS bookmark\n" +
+    @Query(value = "SELECT ranksql.rank AS rank, ranksql.streamer_id AS streamerId, ranksql.name, ranksql.platform, ranksql.profile_url AS profileUrl, ranksql.viewer_cnt AS topViewer, case when b.bookmark_id > 0 then 'TRUE' else 'FALSE' END AS bookmark\n" +
             "FROM \n" +
             "(SELECT ROW_NUMBER() OVER(ORDER BY viewer_cnt DESC) AS rank, live.live_id, live.streamer_id, live.live_origin_id, live.stream_url, live.thumbnail_url, live_log.live_log_id ,live_log.cycle_log_id, live_log.category_id, live_log.title, round(max(live_log.viewer_cnt),0) AS viewer_cnt, s.name, s.platform, s.profile_url\n" +
             "FROM lives AS live\n" +
