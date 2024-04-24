d9b5f31bfd9e84aba6d5b80a3f0d4abe5c3114c8
9gi_coach21 <9gi_coach21@ssafy.com>
2024-04-24 16:33:56 +0900

    feat: 사이클 데이터 날짜 타입 변경

--- crawling/services/cycle_logs.py
+++ crawling/services/cycle_logs.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-import datetime
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from models.cycle_logs import CycleLog
 from schemas.cycle_logs import CycleLogCreate
 
@@ -10,7 +11,7 @@ class CycleLogService:
         db_cycle_log = CycleLog(
             afreeca_viewer_cnt=cycle_log.afreeca_viewer_cnt,
             chzzk_viewer_cnt=cycle_log.chzzk_viewer_cnt,
-            cycle_dt=datetime.datetime.now()
+            cycle_dt=datetime.now(ZoneInfo('Asia/Seoul'))
         )
         db.add(db_cycle_log)
         db.commit()
