563334df293588728476d65d52c21901f4ced0a9
kwonhyeokgeun <xkhg0611x@naver.com>
2024-05-06 17:28:02 +0900

    feature : 북마크 뷰로그 인가 처리 완료

--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/global/security/JwtAuthenticationFilter.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/global/security/JwtAuthenticationFilter.java
@@ -13,7 +13,9 @@ import lombok.RequiredArgsConstructor;
 import org.greenpine.cheeseballoon.global.token.JwtUtil;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -21,12 +23,15 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.security.SignatureException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
-
+    List<GrantedAuthority> userAuth = Collections.singletonList(new SimpleGrantedAuthority("USER"));
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, JwtException {
         String token = resolveToken(request);
@@ -39,9 +44,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             Long memberId = jwtUtil.getUserId(claims);
             //System.out.println("필터 memberId : " + memberId);
 
-            //memberId 담고 인증 부여
+            //memberId 담고 user권한 부여
             AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    memberId, null, AuthorityUtils.NO_AUTHORITIES); //일단은 빈권한 부여 (List<GrantedAuthority> 타입으로 부여)
+                    memberId, null, userAuth);
+
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
