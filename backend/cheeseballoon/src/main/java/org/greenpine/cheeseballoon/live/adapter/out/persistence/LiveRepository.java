9fd9ec00672f929694fd09227bf75e8643dc6171
sangjae <dlbia2009@gmail.com>
2024-05-09 02:37:48 +0900

    feature : 시청률 랭킹 API 기능 추가

--- backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/live/adapter/out/persistence/LiveRepository.java
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/live/adapter/out/persistence/LiveRepository.java
@@ -2,6 +2,7 @@ package org.greenpine.cheeseballoon.live.adapter.out.persistence;
 
 import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindAvgViewerRankResDtoInterface;
 import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindFollowerRankResDtoInterface;
+import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindRatingRankResDtoInterface;
 import org.greenpine.cheeseballoon.ranking.application.port.out.dto.FindTopViewerRankResDtoInterface;
 import org.greenpine.cheeseballoon.streamer.adapter.out.persistence.StreamerEntity;
 import org.springframework.data.domain.Page;
@@ -86,6 +87,7 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
             "ON ranksql.streamer_id = b.streamer_id", nativeQuery = true)
     List<FindTopViewerRankResDtoInterface> findTopViewerRankingByPlatform(LocalDateTime beforeDay, LocalDateTime today, char platform, Long memberId);
 
+    // 팔로워 랭킹 조회
     @Query(value = "SELECT ROW_NUMBER() OVER(ORDER BY r.follower DESC, r.name ASC) AS rank, case when b.bookmark_id IS NOT NULL then 'TRUE' ELSE 'FALSE' END AS bookmark, r.streamer_id AS streamerId, r.name, r.profile_url AS profileUrl, r.platform, r.follower\n" +
             "FROM (SELECT * FROM bookmarks WHERE member_id = :memberId) AS b\n" +
             "right outer JOIN (SELECT s.streamer_id, s.name, s.profile_url, s.platform, sl.follower FROM streamers  AS s,\n" +
@@ -95,6 +97,7 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
             "ORDER BY r.follower DESC", nativeQuery = true)
     List<FindFollowerRankResDtoInterface> findFollowerRanking(LocalDateTime beforeDay, LocalDateTime today, Long memberId);
 
+    // 팔로워 랭킹 플랫폼 포함 조회
     @Query(value = "SELECT ROW_NUMBER() OVER(ORDER BY r.follower DESC, r.name ASC) AS rank, case when b.bookmark_id IS NOT NULL then 'TRUE' ELSE 'FALSE' END AS bookmark, r.streamer_id AS streamerId, r.name, r.profile_url AS profileUrl, r.platform, r.follower\n" +
             "FROM (SELECT * FROM bookmarks WHERE member_id = :memberId) AS b\n" +
             "right outer JOIN (SELECT s.streamer_id, s.name, s.profile_url, s.platform, sl.follower FROM streamers  AS s,\n" +
@@ -103,5 +106,27 @@ public interface LiveRepository extends JpaRepository<LiveEntity,Long> {
             "ON b.streamer_id = r.streamer_id\n" +
             "ORDER BY r.follower DESC", nativeQuery = true)
     List<FindFollowerRankResDtoInterface> findFollowerRankingByPlatform(LocalDateTime beforeDay, LocalDateTime today, char platform, Long memberId);
+
+    // 시청률 랭킹 조회
+    @Query(value = "SELECT ROW_NUMBER() OVER (ORDER BY rating DESC) AS RANK, rating.streamer_id AS streamerId, name, platform, profile_url AS profileUrl, case when bookmark_id IS NULL then 'false' ELSE 'true' END AS bookmark, ROUND(rating, 2) as rating FROM\n" +
+            "(SELECT s.streamer_id, s.name, s.profile_url, s.channel_url, s.platform, rating FROM\n" +
+            "((SELECT l.streamer_id, viewer_cnt, (viewer_cnt / (afreeca_viewer_cnt + chzzk_viewer_cnt)) * 100 AS rating FROM lives AS l \n" +
+            "JOIN (SELECT ll.live_id, ll.viewer_cnt, ll.cycle_log_id, cycle.afreeca_viewer_cnt, chzzk_viewer_cnt, cycle.cycle_dt FROM live_logs AS ll \n" +
+            "JOIN (SELECT * FROM cycle_logs WHERE cycle_dt BETWEEN :beforeDay AND :today) AS cycle ON ll.cycle_log_id = cycle.cycle_log_id) AS result \n" +
+            "ON result.live_id = l.live_id GROUP BY streamer_id) AS r JOIN\n" +
+            "(SELECT * FROM streamers) AS s ON s.streamer_id = r.streamer_id ) LIMIT 300) AS rating LEFT OUTER JOIN\n" +
+            "(SELECT bookmark_id, streamer_id FROM bookmarks WHERE member_id = :memberId) as book ON book.streamer_id = rating.streamer_id", nativeQuery = true)
+    List<FindRatingRankResDtoInterface> findRatingRanking(LocalDateTime beforeDay, LocalDateTime today, Long memberId);
+
+    // 시청률 랭킹 플랫폼 포함 조회
+    @Query(value = "SELECT ROW_NUMBER() OVER (ORDER BY rating DESC) AS RANK, rating.streamer_id AS streamerId, name, platform, profile_url AS profileUrl, case when bookmark_id IS NULL then 'false' ELSE 'true' END AS bookmark, ROUND(rating, 2) as rating FROM\n" +
+            "(SELECT s.streamer_id, s.name, s.profile_url, s.channel_url, s.platform, rating FROM\n" +
+            "((SELECT l.streamer_id, viewer_cnt, (viewer_cnt / (afreeca_viewer_cnt + chzzk_viewer_cnt)) * 100 AS rating FROM lives AS l \n" +
+            "JOIN (SELECT ll.live_id, ll.viewer_cnt, ll.cycle_log_id, cycle.afreeca_viewer_cnt, chzzk_viewer_cnt, cycle.cycle_dt FROM live_logs AS ll \n" +
+            "JOIN (SELECT * FROM cycle_logs WHERE cycle_dt BETWEEN :beforeDay AND :today) AS cycle ON ll.cycle_log_id = cycle.cycle_log_id) AS result \n" +
+            "ON result.live_id = l.live_id GROUP BY streamer_id) AS r JOIN\n" +
+            "(SELECT * FROM streamers WHERE platform = :platform) AS s ON s.streamer_id = r.streamer_id ) LIMIT 300) AS rating LEFT OUTER JOIN\n" +
+            "(SELECT bookmark_id, streamer_id FROM bookmarks WHERE member_id = :memberId) as book ON book.streamer_id = rating.streamer_id", nativeQuery = true)
+    List<FindRatingRankResDtoInterface> findRatingRankingByPlatform(LocalDateTime beforeDay, LocalDateTime today, char platform, Long memberId);
 
 }
