8ae614626694add9d8f25fc8ed63bd4dc6c6dcfb
9gi_coach21 <9gi_coach21@ssafy.com>
2024-05-08 11:07:05 +0900

    feat: 치지직 크롤링 에러 해결

--- crawling/schedulers/crawlers.py
+++ crawling/schedulers/crawlers.py
@@ -43,7 +43,7 @@ class Scheduler:
             CrawlingBusiness().follow_crawling,
             # trigger=IntervalTrigger(seconds=30),
             # trigger=IntervalTrigger(seconds=600, start_date=datetime.now(ZoneInfo('Asia/Seoul'))),
-            trigger=CronTrigger(hour=10, minute=0),
+            trigger=CronTrigger(hour=11, minute=20),
             misfire_grace_time=None,
             max_instances=5,
             id='follower_start',
