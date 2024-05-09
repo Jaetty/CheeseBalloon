5bad00c878674e163cef50534890a9ab8aa06a3e
9gi_coach21 <9gi_coach21@ssafy.com>
2024-05-09 09:45:26 +0900

    feat: startup 주석 변경

--- crawling/main.py
+++ crawling/main.py
@@ -10,11 +10,11 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 # app.include_router(users.router)
 
-# @app.on_event("startup")
-# async def startup_scheduler():
-#     db = next(get_db())
-#     Scheduler().start(db=db)
-#     Scheduler().follower_start(db=db)
+@app.on_event("startup")
+async def startup_scheduler():
+    db = next(get_db())
+    Scheduler().start(db=db)
+    Scheduler().follower_start(db=db)
 
 
 @app.get("/")
