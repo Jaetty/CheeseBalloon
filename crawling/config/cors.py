2d017acef4c15f98054e2ce500fa644cd7c553a1
rudgns9334 <rudgns9334@gmail.com>
2024-05-13 21:02:10 +0900

    feat: cors * 설정 및 none image 설정

--- crawling/config/cors.py
+++ crawling/config/cors.py
@@ -4,8 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://cheeseballoon.site", "https://cheeseballoon.site"],
-        # allow_origins=["*"],
+        # allow_origins=["http://cheeseballoon.site", "https://cheeseballoon.site"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
