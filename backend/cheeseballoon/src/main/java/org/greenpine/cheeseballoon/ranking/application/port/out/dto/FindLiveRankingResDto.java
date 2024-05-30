fb027f1ab3180a707751f7ed7cc5fd24bfdc6a40
kwonhyeokgeun <xkhg0611x@naver.com>
2024-05-30 22:56:43 +0900

    feature : live ranking init

---
+++ backend/cheeseballoon/src/main/java/org/greenpine/cheeseballoon/ranking/application/port/out/dto/FindLiveRankingResDto.java
@@ -0,0 +1,26 @@
+package org.greenpine.cheeseballoon.ranking.application.port.out.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindLiveRankingResDto {
+    Long streamId;
+    Long liveId;
+    Long liveLogId;
+    String name;
+    String title;
+    String thumbnailUrl;
+    char platform;
+    String profileUrl;
+    String category;
+    int viewerCnt;
+    String streamUrl;
+    String channelUrl;
+    Boolean bookmark;
+}
