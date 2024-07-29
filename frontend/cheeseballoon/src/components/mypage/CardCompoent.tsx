f7523ea3ecce33dc7aa8660d2b156d96ec126ae2
myikn <myikn.999@gmail.com>
2024-07-29 18:57:13 +0900

    Feat: 마이페이지 즐겨찾기 수정

--- frontend/cheeseballoon/src/components/mypage/CardCompoent.tsx
+++ frontend/cheeseballoon/src/components/mypage/CardCompoent.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "src/components/mypage/CardComponent.module.scss";
 import Image from "next/image";
-import { LiveData } from "src/types/type";
+import { FavState } from "src/types/type";
 import aflogo from "src/stores/afreeca.ico";
 import chzlogo from "public/svgs/chzzk.svg";
 import fav from "public/svgs/fav.svg";
@@ -9,13 +9,34 @@ import nofav from "public/svgs/nofav.svg";
 import Link from "next/link";
 
 type CardProps = {
-  data: LiveData;
+  data: FavState;
 };
 
 export default function Card({ data }: CardProps) {
   const [isFav, setIsFav] = useState(true);
 
   const handleFavClick = () => {
+    if (isFav) {
+      fetch(`${process.env.NEXT_PUBLIC_MYPAGE_BOOK}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TEST_AUTH}`,
+        },
+        body: JSON.stringify({
+          streamerId: data.streamerId,
+        }),
+      });
+    } else {
+      fetch(`${process.env.NEXT_PUBLIC_MYPAGE_BOOK}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TEST_AUTH}`,
+        },
+        body: JSON.stringify({
+          streamerId: data.streamerId,
+        }),
+      });
+    }
     setIsFav((prev) => !prev);
   };
 
@@ -46,7 +67,7 @@ export default function Card({ data }: CardProps) {
         />
       </div>
       <div className={styles.cardname}>
-        <Link href={`/detail/${data.streamId}`} className={styles.link}>
+        <Link href={`/detail/${data.streamerId}`} className={styles.link}>
           {data.name}
         </Link>
         <span className={styles.logo}>
@@ -57,7 +78,22 @@ export default function Card({ data }: CardProps) {
           )}
         </span>
       </div>
-      <div className={styles.cardfollowers}>{data.viewerCnt} followers</div>
+      <div className={styles.buttonsWrapper}>
+        {data.isLive ? (
+          <Link
+            href={`/detail/${data.streamerId}`}
+            className={styles.link}
+            passHref
+          >
+            <div className={styles.liveButton}>LIVE</div>
+          </Link>
+        ) : (
+          <div className={styles.disabledButton}>LIVE</div>
+        )}
+        <Link href="/detail/3" className={styles.link}>
+          <div className={styles.detailButton}>상세</div>
+        </Link>
+      </div>
     </div>
   );
 }
