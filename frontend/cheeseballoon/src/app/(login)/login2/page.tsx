3b807ebeb8335ceb91eac2bb67fbef32962cf5bc
Suou <baewc95@gmail.com>
2024-08-02 17:11:06 +0900

    fix: 환경변수명 수정 NEXT_PUBLIC 접두사 추가

--- frontend/cheeseballoon/src/app/(login)/login2/page.tsx
+++ frontend/cheeseballoon/src/app/(login)/login2/page.tsx
@@ -3,29 +3,11 @@
 /* eslint-disable camelcase */
 
 import { signIn, signOut } from "next-auth/react";
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/navigation";
 import logo from "public/svgs/logo.png";
 import styles from "src/app/(login)/login2/page.module.scss";
 
 export default function Login() {
-  const redirectUrl = "https://cheeseballoon.site/detail/1234";
-  // const [isLoggedIn, setIsloggedIn] = useState<boolean>(true);
-  // const router = useRouter();
-  // const { data: session, status } = useSession();
-
-  // useEffect(() => {
-  //   // console.log(session);
-  //   // console.log(status);
-  //   // if (session.data) {
-  //   //   router.push("/home");
-  //   // } else {
-  //   //   setIsloggedIn(false);
-  //   // }
-  // }, [router, session, status]);
-
   return (
-    // !isLoggedIn &&
     <div className={styles.login}>
       <div className={styles.left}>
         <video className={styles.video} muted autoPlay loop>
