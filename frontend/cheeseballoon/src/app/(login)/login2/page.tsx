54c803bcf6a7fb470fd84639ea9abc3ad0a46ccf
Suou <baewc95@gmail.com>
2024-08-05 19:46:00 +0900

    fix: next-auth 삭제

--- frontend/cheeseballoon/src/app/(login)/login2/page.tsx
+++ frontend/cheeseballoon/src/app/(login)/login2/page.tsx
@@ -2,8 +2,6 @@
 
 /* eslint-disable camelcase */
 
-// import { signIn, signOut } from "next-auth/react";
-import { signInWithGoogle } from "@/src/serverActions/auth";
 import logo from "public/svgs/logo.png";
 import styles from "src/app/(login)/login2/page.module.scss";
 
@@ -23,44 +21,6 @@ export default function Login() {
         <div className={styles.social_login}>
           <div className={styles.sub}>소셜 로그인</div>
           <div className={styles.login_button}>
-            <form action={signInWithGoogle}>
-              <button type="submit">구글 로그인</button>
-            </form>
-            {/* <div>
-              <button
-                type="button"
-                onClick={() =>
-                  signIn("google")
-                }
-              >
-                구글 로그인
-              </button>
-            </div> */}
-            {/* <div>
-              <button
-                type="button"
-                onClick={() =>
-                  signIn("kakao")
-                }
-              >
-                카카오 로그인
-              </button>
-            </div>
-            <div>
-              <button
-                type="button"
-                onClick={() =>
-                  signIn("naver")
-                }
-              >
-                네이버 로그인
-              </button>
-            </div> */}
-            {/* <div>
-              <button type="button" onClick={() => signOut()}>
-                로그아웃
-              </button>
-            </div> */}
           </div>
         </div>
       </div>
