cc14b91a6e69b9727acf825c4732fff1c3186444
Suou <baewc95@gmail.com>
2024-08-11 15:55:24 +0900

    feat: 구글 로그인

--- frontend/cheeseballoon/src/app/(login)/login/page.tsx
+++ frontend/cheeseballoon/src/app/(login)/login/page.tsx
@@ -5,15 +5,21 @@ import kakaoLogin from "src/stores/kakao_login_large_narrow.png";
 import logo from "public/svgs/logo.png";
 import styles from "src/app/(login)/login/page.module.scss";
 
+const googleRestApiKey = process.env.NEXT_PUBLIC_GOOGLE_REST_API;
+const googleRedirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
+const kakaoRestApiKey = process.env.NEXT_PUBLIC_KAKAO_REST_API;
+const kakaoRedirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+
 export default function Login() {
   const router = useRouter();
-  const restApiKey = process.env.NEXT_PUBLIC_KAKAO_REST_API;
-  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-  // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${restApiKey}&redirect_uri=${redirectUri}&response_type=code`;
-  const handleLogin = () => {
-    // event.preventDefault();
-    router.replace(kakaoURL);
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApiKey}&redirect_uri=${kakaoRedirectUri}&response_type=code`;
+  const googleURL = `https://accounts.google.com/o/oauth2/auth?response_type=code&scope=email+profile&client_id=${googleRestApiKey}&redirect_uri=${googleRedirectUri}`;
+  const handleLogin = (provider: string) => {
+    if (provider === "google") {
+      router.replace(googleURL);
+    } else if (provider === "kakao") {
+      router.replace(kakaoURL);
+    }
   };
 
   return (
@@ -31,11 +37,14 @@ export default function Login() {
         <div className={styles.social_login}>
           <div className={styles.sub}>소셜 로그인</div>
           <div className={styles.login_button}>
+            <button type="button" onClick={() => handleLogin("google")}>
+              구글 로그인
+            </button>
             <img
               className={styles.login_img}
               src={kakaoLogin.src}
               alt="ss"
-              onClick={handleLogin}
+              onClick={() => handleLogin("kakao")}
               role="presentation"
             />
           </div>
--- frontend/cheeseballoon/src/app/api/member/login/[provider]/page.tsx
+++ frontend/cheeseballoon/src/app/api/member/login/[provider]/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 import { accessTokenState } from "@/src/stores/store";
 
 export default function KakaoRedirect() {
   const router = useRouter();
+  const { provider } = useParams();
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
-  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+  const googleRedirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
+  const kakaoRedirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
   const { accessToken, setAccessToken } = accessTokenState.getState();
 
   useEffect(() => {
     const getToken = async () => {
       try {
+        let redirectUri;
+        if (provider === "google") {
+          redirectUri = googleRedirectUri;
+        } else if (provider === "kakao") {
+          redirectUri = kakaoRedirectUri;
+        }
         const response = await fetch(`${redirectUri}/code?code=${code}`);
         const data = await response.json();
 
@@ -29,15 +37,23 @@ export default function KakaoRedirect() {
         router.back();
       } catch (error) {
         // eslint-disable-next-line no-alert
-        alert("로그인이 실패했습니다");
-        router.replace("/login");
+        // alert("로그인이 실패했습니다");
+        // router.replace("/login");
       }
     };
 
     if (code) {
       getToken();
     }
-  }, [code, redirectUri, router, setAccessToken, accessToken]);
+  }, [
+    code,
+    router,
+    setAccessToken,
+    accessToken,
+    googleRedirectUri,
+    kakaoRedirectUri,
+    provider,
+  ]);
 
   return <div></div>;
 }
