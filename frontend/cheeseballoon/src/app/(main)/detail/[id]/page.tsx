7e4ec1492b311f5fffa5357b24788b0238ba4005
Suou <baewc95@gmail.com>
2024-08-11 15:08:47 +0900

    feat: 메타데이터 title 변경

--- frontend/cheeseballoon/src/app/(main)/detail/[id]/page.tsx
+++ frontend/cheeseballoon/src/app/(main)/detail/[id]/page.tsx
@@ -4,17 +4,21 @@ import style from "src/app/(main)/detail/[id]/page.module.scss";
 
 const STREAMER_API_URL = process.env.NEXT_PUBLIC_STREAMER_API_URL;
 
+export const getData = async (url: string) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
 export const generateMetadata = async ({
   params,
 }: {
   params: { id: string };
 }): Promise<Metadata> => {
-  const response = await fetch(`${STREAMER_API_URL}${params.id}`);
-  const streamerData = await response.json();
+  const streamerData = await getData(`${STREAMER_API_URL}${params.id}`);
 
   return {
-    title: `${streamerData.name}`,
-    description: `${streamerData.name}의 방송기록`,
+    title: `${streamerData.data.name}`,
+    description: `${streamerData.data.name}의 방송기록`,
   };
 };
 
