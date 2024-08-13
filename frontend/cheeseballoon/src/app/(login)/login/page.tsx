7acb0cddd0b7b3d433e2f47f28f01674c98639e2
Suou <baewc95@gmail.com>
2024-08-13 12:24:28 +0900

    feat: 소셜 로그인 버튼

--- frontend/cheeseballoon/src/app/(login)/login/page.tsx
+++ frontend/cheeseballoon/src/app/(login)/login/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import kakaoLogin from "src/stores/kakao_login_large_narrow.png";
 import logo from "public/svgs/logo.png";
 import styles from "src/app/(login)/login/page.module.scss";
+import googleLogo from "src/stores/google_logo.png";
+import naverLogo from "src/stores/naver_logo.png";
+import kakaoLogo from "src/stores/kakao_logo.png";
 
 const googleRestApiKey = process.env.NEXT_PUBLIC_GOOGLE_REST_API;
 const googleRedirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
 const kakaoRestApiKey = process.env.NEXT_PUBLIC_KAKAO_REST_API;
 const kakaoRedirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
 
+const logos = {
+  google: googleLogo.src,
+  naver: naverLogo.src,
+  kakao: kakaoLogo.src,
+};
+
 export default function Login() {
   const router = useRouter();
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApiKey}&redirect_uri=${kakaoRedirectUri}&response_type=code`;
   const googleURL = `https://accounts.google.com/o/oauth2/auth?response_type=code&scope=email+profile&client_id=${googleRestApiKey}&redirect_uri=${googleRedirectUri}`;
+
   const handleLogin = (provider: string) => {
     if (provider === "google") {
       router.replace(googleURL);
@@ -21,6 +30,31 @@ export default function Login() {
       router.replace(kakaoURL);
     }
   };
+
+  function loginButton(provider: string, ko: string) {
+    return (
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => handleLogin(provider)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleLogin(provider);
+          }
+        }}
+        className={`${styles["login-button-wrapper"]} ${styles[provider]}`}
+      >
+        <div className={styles["login-button-logo"]}>
+          <img
+            src={logos[provider as keyof typeof logos]}
+            alt={`${provider}`}
+            className={styles.logo}
+          />
+        </div>
+        <div className={styles["login-button-text"]}>{ko} 로그인</div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.login}>
@@ -36,14 +70,10 @@ export default function Login() {
         </div>
         <div className={styles.social_login}>
           <div className={styles.sub}>소셜 로그인</div>
-          <div className={styles.login_button}>
-            <img
-              className={styles.login_img}
-              src={kakaoLogin.src}
-              alt="ss"
-              onClick={() => handleLogin("kakao")}
-              role="presentation"
-            />
+          <div className={styles["login-button"]}>
+            {loginButton("google", "구글")}
+            {/* {loginButton("naver", "네이버")} */}
+            {loginButton("kakao", "카카오")}
           </div>
         </div>
       </div>
