709c63dc74d854e0894f475d915fcd93d3fc5274
Suou <baewc95@gmail.com>
2024-09-02 20:30:21 +0900

    fix: 로그인 페이지 접근 제한

--- frontend/cheeseballoon/src/middleware.ts
+++ frontend/cheeseballoon/src/middleware.ts
@@ -9,11 +9,15 @@ export default function middleware(request: NextRequest) {
   const response = NextResponse.next();
   response.cookies.set("viewport", viewport, { path: "/" });
 
-  if (pathname === "/login" || pathname.startsWith("/member")) {
-    if (refreshToken) {
-      return NextResponse.redirect(new URL("/home", request.url));
-    }
-    return response;
+  // if (pathname === "/login" || pathname.startsWith("/member")) {
+  //   if (refreshToken) {
+  //     return NextResponse.redirect(new URL("/home", request.url));
+  //   }
+  //   return response;
+  // }
+
+  if (pathname === "/login") {
+    return NextResponse.redirect(new URL("/home", request.url));
   }
 
   return response;
