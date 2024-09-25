151c538337a783d955c46d94445ad9abdaf21b03
Suou <baewc95@gmail.com>
2024-09-25 19:01:25 +0900

    feat: 로그인 기능 제한

--- frontend/cheeseballoon/src/middleware.ts
+++ frontend/cheeseballoon/src/middleware.ts
@@ -16,9 +16,9 @@ export default function middleware(request: NextRequest) {
     return response;
   }
 
-  // if (pathname === "/login") {
-  //   return NextResponse.redirect(new URL("/home", request.url));
-  // }
+  if (pathname === "/login") {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
 
   return response;
 }
